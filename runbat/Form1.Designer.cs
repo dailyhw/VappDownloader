3a87e1e5ce3d9a1c0390dd53be68fb5d06c01cb6
unknown <>
2017-09-15 23:16:38 +0900

    packages

---
+++ runbat/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace runbat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdSource = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Link = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Exit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAttention = new System.Windows.Forms.Button();
+            this.lbProcIndex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(545, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(133, 12);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(406, 21);
+            this.txtSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output Folder";
+            // 
+            // Link
+            // 
+            this.Link.AutoSize = true;
+            this.Link.Location = new System.Drawing.Point(12, 51);
+            this.Link.Name = "Link";
+            this.Link.Size = new System.Drawing.Size(70, 12);
+            this.Link.TabIndex = 3;
+            this.Link.Text = "M3U8 Links";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 75);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(606, 200);
+            this.textBox2.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 281);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(606, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(545, 310);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(14, 310);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Create by 자두같이생긴애";
+            // 
+            // btnAttention
+            // 
+            this.btnAttention.Location = new System.Drawing.Point(402, 46);
+            this.btnAttention.Name = "btnAttention";
+            this.btnAttention.Size = new System.Drawing.Size(64, 23);
+            this.btnAttention.TabIndex = 9;
+            this.btnAttention.Text = "주의사항";
+            this.btnAttention.UseVisualStyleBackColor = true;
+            this.btnAttention.Click += new System.EventHandler(this.btnAttention_Click);
+            // 
+            // lbProcIndex
+            // 
+            this.lbProcIndex.AutoSize = true;
+            this.lbProcIndex.Location = new System.Drawing.Point(252, 310);
+            this.lbProcIndex.Name = "lbProcIndex";
+            this.lbProcIndex.Size = new System.Drawing.Size(111, 12);
+            this.lbProcIndex.TabIndex = 10;
+            this.lbProcIndex.Text = "( 1 / 10 ) 진행 중...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 341);
+            this.Controls.Add(this.lbProcIndex);
+            this.Controls.Add(this.btnAttention);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Link);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lovelyz Vapp Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Link;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAttention;
+        private System.Windows.Forms.Label lbProcIndex;
+    }
+}
+
---
+++ runbat/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using runbat.Properties;
+using System.Threading;
+using System.Net;
+
+namespace runbat
+{
+    public partial class Form1 : Form
+    {
+        WebClient wc;
+        Process prcFFMPEG = new Process();
+        List<string> titles = new List<string>();
+        Form dlg;
+        int nProcessingCount;
+        int nProcessingMaxCount;
+
+        enum ProcState
+        {
+            PROC_STATE_READY,
+            PROC_STATE_ENCORDING,
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            StateChange(ProcState.PROC_STATE_READY);
+            txtSource.Text = Application.StartupPath;
+        }
+
+        private void StateChange(ProcState state)
+        {
+            switch(state)
+            {
+                case ProcState.PROC_STATE_READY:
+                    lbProcIndex.Text = "대 기 중";
+                    break;
+                case ProcState.PROC_STATE_ENCORDING:
+                    lbProcIndex.Text = string.Format("( {0} / {1} ) 진행 중 ...", nProcessingCount+1, nProcessingMaxCount);
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Settings.Default.SourceDir) == false)
+            {
+                fbdSource.SelectedPath = Settings.Default.SourceDir;
+            }
+
+            if (fbdSource.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSource.Text = fbdSource.SelectedPath;
+                Settings.Default.SourceDir = txtSource.Text;
+                Settings.Default.Save();
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            this.btnStart.Enabled = false;
+            nProcessingCount = 0;
+            this.progressBar1.Value = 0;
+            titles.Clear();
+
+            if (wc == null)
+                wc = new WebClient();
+
+            string[] split_string = textBox2.Text.Split('\n');
+            
+            foreach (string titleUrl in split_string)
+            {
+                if (titleUrl.Contains("http") == false)
+                    continue;
+
+                string[] chk = titleUrl.Split('|');
+                titles.Add(checkExpression(chk[0]));
+
+                DownloadFile(nProcessingCount++, chk[1]);
+            }
+
+            nProcessingCount = 0;
+            nProcessingMaxCount = titles.Count;
+            ThreadPool.QueueUserWorkItem((object state) =>
+            {
+                ConvertFile(txtSource.Text + "/", titles[0], Application.StartupPath + "/" + nProcessingCount.ToString(), nProcessingCount);
+                nProcessingCount++;
+            });
+        }
+
+        private string checkExpression(string arg)
+        {
+            string result = arg;
+
+            if (result.Contains('\\'))
+                result = result.Replace('\\', '.');
+            if (result.Contains('/'))
+                result = result.Replace('/', '.');
+            if (result.Contains(':'))
+                result = result.Replace(':', '.');
+            if (result.Contains('*'))
+                result = result.Replace('*', '.');
+            if (result.Contains('?'))
+                result = result.Replace('?', '.');
+            if (result.Contains('"'))
+                result = result.Replace('"', '.');
+            if (result.Contains('<'))
+                result = result.Replace('<', '.');
+            if (result.Contains('>'))
+                result = result.Replace('>', '.');
+            if (result.Contains('|'))
+                result = result.Replace('|', '.');
+
+            return result;
+        }
+
+        private void DownloadFile(int index, string url)
+        {
+            byte[] streamData = wc.DownloadData(url);
+            string strData = Encoding.Default.GetString(streamData);
+
+            string[] chkString = url.Split('?');
+            chkString[0] = chkString[0].Substring(0, chkString[0].Length - 5);
+            string[] subChk = chkString[0].Split('/');
+
+
+            chkString[1] = ".ts?" + chkString[1];
+
+            strData = strData.Replace(subChk[subChk.Length - 1], chkString[0]);
+            strData = strData.Replace(".ts", chkString[1]);
+
+            System.IO.File.WriteAllText(Application.StartupPath + "/" + index.ToString() + ".m3u8", strData, Encoding.Default);
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Process[] procs = Process.GetProcessesByName("ffmpeg");
+            if (procs.Length > 0)
+            {
+                foreach (Process proc in Process.GetProcessesByName("ffmpeg"))
+                    proc.Kill();
+            }
+
+            Application.Exit();
+        }
+
+        private void ConvertFile(string filePath, string fileName, string srcFile, int srcIndex)
+        {
+            this.BeginInvoke(new MethodInvoker(() =>
+            {
+                StateChange(ProcState.PROC_STATE_ENCORDING);
+            }));
+
+            System.IO.FileInfo checkFile = new System.IO.FileInfo(filePath + fileName + ".ts");
+            if (checkFile.Exists)
+                System.IO.File.Delete(filePath + fileName + ".ts");
+
+            try
+            {
+                string dstFile = filePath + fileName;
+                string strFFMPEGOut;
+                ProcessStartInfo psiProcInfo = new ProcessStartInfo();
+                TimeSpan estimatedTime = TimeSpan.MaxValue;
+
+                StreamReader srFFMPEG;
+                string strFFMPEGCmd = " -c copy \"" + dstFile + ".ts" + "\" " + "-protocol_whitelist \"file,http,https,tcp,tls\" -i " + "\"" + srcFile + ".m3u8" + "\"";
+
+                psiProcInfo.FileName = Application.StartupPath + ((IntPtr.Size == 8) ? "\\x64" : "\\x86") + "\\ffmpeg.exe";
+                psiProcInfo.Arguments = strFFMPEGCmd;
+                psiProcInfo.UseShellExecute = false;
+                psiProcInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                psiProcInfo.RedirectStandardError = true;
+                psiProcInfo.RedirectStandardOutput = true;
+                psiProcInfo.CreateNoWindow = true;
+
+                prcFFMPEG.StartInfo = psiProcInfo;
+
+                prcFFMPEG.Start();
+
+                srFFMPEG = prcFFMPEG.StandardError;
+
+                do
+                {
+                    strFFMPEGOut = srFFMPEG.ReadLine();
+
+                    string duration = "Duration";
+                    if (strFFMPEGOut != null)
+                    {
+                        if (strFFMPEGOut.TrimStart().IndexOf(duration) == 0)
+                        {
+                            try
+                            {
+                                string text = strFFMPEGOut.TrimStart().Substring(duration.Length + 2);
+                                int pos = text.IndexOf(",");
+                                string estimated = text.Substring(0, pos);
+
+                                estimatedTime = TimeSpan.Parse(estimated);
+                            }
+                            catch
+                            {
+                            }
+                        }
+
+                        if (estimatedTime != TimeSpan.MaxValue)
+                        {
+                            // 예측 시간이 나왔으면.
+                            string time = "time=";
+                            int startPos = strFFMPEGOut.IndexOf(time);
+                            if (startPos != -1)
+                            {
+                                string text = strFFMPEGOut.Substring(startPos + time.Length);
+                                int pos = text.IndexOf(" ");
+                                string current = text.Substring(0, pos);
+
+                                TimeSpan currentTime = TimeSpan.Parse(current);
+
+                                int progresss = (int)(currentTime.TotalMilliseconds * 100 / estimatedTime.TotalMilliseconds);
+                                this.BeginInvoke(new MethodInvoker(() =>
+                                {
+                                    this.progressBar1.Value = progresss;
+                                }));
+                            }
+                        }
+                    }
+                } while (prcFFMPEG.HasExited == false);
+
+            }
+            finally
+            {
+                this.BeginInvoke(new MethodInvoker(() =>
+                {
+                    // 현재 처리한 타이틀 제거
+                    if (titles.Count > 0)
+                        titles.RemoveAt(0);
+
+                    // m3u8 파일 삭제
+                    System.IO.FileInfo deleteSrc = new System.IO.FileInfo(Application.StartupPath + "/" + srcIndex.ToString() + ".m3u8");
+                    if (deleteSrc.Exists)
+                        System.IO.File.Delete(Application.StartupPath + "/" + (nProcessingCount - 1).ToString() + ".m3u8");
+
+                    // 남은 타이틀이 없으면 버튼 활성화, 프로세스 처리 완전 종료
+                    if (titles.Count == 0)
+                    {
+                        this.btnStart.Enabled = true;
+                        this.BeginInvoke(new MethodInvoker(() =>
+                        {
+                            StateChange(ProcState.PROC_STATE_READY);
+                        }));
+                    }
+                    else
+                    {
+                        //남은게 있으면 recursion
+                        ThreadPool.QueueUserWorkItem((object state) =>
+                        {
+                            ConvertFile(filePath, titles[0], Application.StartupPath + "/" + nProcessingCount.ToString(), nProcessingCount);
+                            nProcessingCount++;
+                        });
+                    }
+                }));
+            }
+        }
+
+        private void btnAttention_Click(object sender, EventArgs e)
+        {
+            if (dlg == null)
+            {
+                dlg = new Form();
+                dlg.Text = "주 의 사 항";
+                dlg.StartPosition = FormStartPosition.CenterParent;
+
+                // 닫기버튼 추가
+                Button btnExit = new Button();
+                btnExit.Text = "닫 기";
+                btnExit.Click += new EventHandler((object obj, EventArgs eArgs) =>
+                {
+                    dlg.Hide();
+                });
+                btnExit.Dock = DockStyle.Bottom;
+                dlg.Controls.Add(btnExit);
+
+                // 약관 라벨 추가
+                Label lbAttention = new Label();
+                System.IO.FileInfo checkAttention = new System.IO.FileInfo(Application.StartupPath + "/attention.txt");
+                if(checkAttention.Exists)
+                    lbAttention.Text = System.IO.File.ReadAllText(Application.StartupPath + "/attention.txt");
+
+                dlg.Controls.Add(lbAttention);
+                lbAttention.Dock = DockStyle.Fill;
+            }
+
+            dlg.ShowDialog();
+        }
+    }
+}
